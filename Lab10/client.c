163538a83777c2b663f5e125c98f58323426fe9e
kangdongkyun <kdk_kang_c@naver.com>
2018-11-30 10:27:34 +0900

    add Lab10

--- Lab10/client.c
+++ Lab10/client.c
@@ -17,52 +17,23 @@ int main(int argc, char* argv[]){
 	int result,flag=0;
 	pid_t pid;	
 	if((wfd = open("./a",O_WRONLY))==-1){perror("error");exit(0);}
-//	switch(pid=fork()){
-//	case -1: perror("fork");exit(1);break;
-//	case 0 : //write
-//		if((wfd=open("./WrPipe",O_WRONLY))==-1){perror("open");exit(0);}
-//		if((write(wfd,&flag,sizeof(int)))==-1){perror("flag");exit(0);}
-//		printf("Please enter formula (ex : 1 + 2 ) : ");
-//		scanf("%d ",&dt.a);
-//		scanf("%c ",&dt.c);
-//		scanf("%d",&dt.b);
-//		flag=1;
-//		if(write(wfd,&flag,sizeof(int))==-1){perror("flag2");exit(0);}
-//		if(write(wfd,&dt,sizeof(struct data))==-1){perror("data");exit(0);}
-//		sleep(1);
-//		break;
-//	default:
-//		if((rfd = open("./RdPipe",O_RDONLY))==-1){perror("open");exit(0);}
-//		if((read(rfd,&result,sizeof(int)))==-1){perror("read");exit(0);}
-//		printf("result : %d",result);
-//		wait(&status);
-//		break;
-//	}
-
-//	if((write(wfd,&flag,sizeof(int)))==-1){
-//		perror("flag");exit(0);
-//	}
 	printf("Please enter formula (ex: 1 + 2) : ");
 	scanf("%d ",&dt.a);
 	scanf("%c ",&dt.c);
 	scanf("%d",&dt.b);
-	printf("%d %c %d\n",dt.a,dt.c,dt.b);
-//	flag = 1;
-//	if(write(wfd,&flag,sizeof(int))==-1){
-//		perror("flag2");exit(0);
-//	}
+//	printf("%d %c %d\n",dt.a,dt.c,dt.b);
 	if(write(wfd,&dt,sizeof(struct data))==-1){
 		perror("data write");
 		exit(0);
 	}
 	sleep(1);
-//	if(read(rfd,&result,sizeof(int))==-1){
-//		perror("read error");
-//		exit(0);
-//	}
-//	printf("result : %d\n", result);
-	close(rfd);
 	close(wfd);
-		
+	if((rfd = open("./b",O_RDONLY))==-1){perror("open");exit(1);}
+	if(read(rfd,&result,sizeof(int))==-1){
+		perror("read error");
+		exit(0);
+	}
+	printf("result : %d\n", result);
+	close(rfd);
 	return 0;
 }
